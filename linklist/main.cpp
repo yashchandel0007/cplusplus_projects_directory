db26900eb8977911c1713dd26587fa026b9243fe
yashchandel0007 <yashchandel007@outlook.com>
2020-02-26 00:36:18 +0530

    duplicate values removing function -a function which can remove all duplicate values in a sorted linklist made for example if the original linklist is 10->20->20->30->30->40 then output will be 10->20->30->40 -display function improved: last node was not being printed in the previous version

--- linklist/main.cpp
+++ linklist/main.cpp
@@ -24,25 +24,49 @@ void push_in_end(Node** head,int data){
     }
 }
 
+void remove_duplicates(Node *head){
+    Node *first = head;
+    Node *second;
+    while(first->next!=NULL){
+        second = first->next;
+        if(second->data == first->data){
+          first->next = second->next;
+        }
+        else{
+            first = first->next;
+        }
+    }
+
+}
+
 void display(Node *head){
     Node* temp = head;
     if(temp==NULL){
         cout<<"List is empty";
     }
-    while(temp->next != NULL){
+    while(temp != NULL){
         cout<<temp->data<<"->";
         temp = temp->next;
     }
+    cout<<endl;
 }
 int main()
 {
     cout << "Hello world!" << endl;
     Node* head = NULL;
     push_in_end(&head,10);
-    push_in_end(&head,10);
-    push_in_end(&head,10);
-    push_in_end(&head,10);
+    push_in_end(&head,20);
+    push_in_end(&head,20);
+    push_in_end(&head,30);
+    push_in_end(&head,40);
+    push_in_end(&head,40);
+
     display(head);
+
+    remove_duplicates(head);
+
+    display(head);
+
     cout<<"This works";
     return 0;
 
