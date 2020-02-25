735262041ffd7503354ec2289eb9add4e85aada7
yashchandel0007 <yashchandel007@outlook.com>
2020-02-26 00:01:59 +0530

    linklist with insert function -push_in_end() function -display() function

--- linklist/main.cpp
+++ linklist/main.cpp
@@ -2,8 +2,48 @@
 
 using namespace std;
 
+class Node{
+    public: int data;
+    public: Node* next;
+};
+
+void push_in_end(Node** head,int data){
+    Node* newNode = new Node();
+    newNode->data=data;
+    newNode->next=NULL;
+
+    if(*head == NULL){
+       *head = newNode;
+    }
+    else{
+        Node* temp = *head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void display(Node *head){
+    Node* temp = head;
+    if(temp==NULL){
+        cout<<"List is empty";
+    }
+    while(temp->next != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+}
 int main()
 {
     cout << "Hello world!" << endl;
+    Node* head = NULL;
+    push_in_end(&head,10);
+    push_in_end(&head,10);
+    push_in_end(&head,10);
+    push_in_end(&head,10);
+    display(head);
+    cout<<"This works";
     return 0;
+
 }
