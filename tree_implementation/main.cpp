0e39d92f03b730986ba64373a71af1987a9df70e
yashchandel0007 <yashchandel007@outlook.com>
2020-02-26 16:07:02 +0530

    tree structure created -no display function programmed -no insert function is coded -manual insertion is being done

--- tree_implementation/main.cpp
+++ tree_implementation/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+class BTNode{
+    public: BTNode* left;
+    public: BTNode* right;
+    public: int data;
+};
+
+BTNode* newNode(int x){    //because it returns the address of the new Node created so that we can later link it to the head
+    BTNode* newNode = new BTNode();
+    if(newNode==NULL){
+        cout<<"memory error";
+        exit(0);
+    }
+    else{
+        newNode->data = x;
+        newNode->left = NULL;
+        newNode->right = NULL;
+    }
+    return newNode;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //cout << "Hello world!" << endl;
+    BTNode *root;
+    root = newNode(2);
+    root->left = newNode(3);
+    root->right = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->left->right->left = newNode(7);
+    root->right->left->right = newNode(8);
+
     return 0;
+
 }
