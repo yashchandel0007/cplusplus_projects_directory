b0d6330c9560206f36d3e8546ca38ee2e07e2d01
yashchandel0007 <yashchandel007@outlook.com>
2020-02-29 20:31:38 +0530

    tree traversal -convention inorder convention preorder -convention postorder -converse inorder -converse preorder -converse postorder

--- tree_implementation/main.cpp
+++ tree_implementation/main.cpp
@@ -31,11 +31,42 @@ void preorder_convention(BTNode *root){
     }
 }
 
+void postorder_convention(BTNode *root){
+    if(root){
+        postorder_convention(root->left);
+        postorder_convention(root->right);
+        cout<<root->data<<"->";
+    }
+}
+
+void inorder_convention(BTNode *root){
+    if(root){
+        inorder_convention(root->left);
+        cout<<root->data<<"->";
+        inorder_convention(root->right);
+    }
+}
+
 void preorder_converse(BTNode *root){
     if(root){
         cout<<root->data<<"->";
-        preorder_convention(root->right);
-        preorder_convention(root->left);
+        preorder_converse(root->right);
+        preorder_converse(root->left);
+    }
+}
+
+void postorder_converse(BTNode *root){
+    if(root){
+        postorder_converse(root->right);
+        postorder_converse(root->left);
+        cout<<root->data<<"->";
+    }
+}
+void inorder_converse(BTNode *root){
+    if(root){
+        inorder_converse(root->right);
+        cout<<root->data<<"->";
+        inorder_converse(root->left);
     }
 }
 
@@ -51,9 +82,19 @@ int main()
     root->left->right->left = newNode(7);
     root->right->left->right = newNode(8);
     cout<<endl;
-    preorder_convention(root);
     cout<<endl;
+    cout<<"Preorder Convention"<<endl;
+    preorder_convention(root);
+    cout<<endl<<"Inorder Convention"<<endl;
+    inorder_convention(root);
+    cout<<endl<<"Postorder Convention"<<endl;
+    postorder_convention(root);
+    cout<<endl<<"Preorder Converse"<<endl;
     preorder_converse(root);
+    cout<<endl<<"Inorder Converse"<<endl;
+    inorder_converse(root);
+    cout<<endl<<"Postorder Converse"<<endl;
+    postorder_converse(root);
     return 0;
 
 }
